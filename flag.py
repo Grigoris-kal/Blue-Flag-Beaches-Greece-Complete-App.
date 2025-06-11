5b82077d01c87de2d8377b3e80890880d42c94e6
Grigoris-kal <grigorisk@hotmail.com>
2025-06-11 19:51:29 +0200

    Update flag.py

--- flag.py
+++ flag.py
@@ -16,6 +16,15 @@ from datetime import datetime
 # ─────────────────────────────────────────────────────────────────────────────
 # 1) Load environment variables and page config
 # ─────────────────────────────────────────────────────────────────────────────
+
+if 'app' not in st.query_params:
+    st.set_page_config(
+        page_title="Blue Flag Beaches of Greece",
+        page_icon="🌊",
+        layout="wide",
+        initial_sidebar_state="collapsed"
+    )
+
 load_dotenv()
 JAWG_TOKEN = os.getenv('JAWG_TOKEN') or "f2wwvI5p3NCM9DJXW3xs7LZLcaY6AM9HKMYxlxdZWOQ9UeuFGirPhlHYpaOcLtLV"
 COPERNICUS_USERNAME = os.getenv('COPERNICUS_USERNAME')
