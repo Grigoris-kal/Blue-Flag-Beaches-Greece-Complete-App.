95ad32c0494f754d26b6d7f263be1bcc99282580
Grigoris-kal <grigorisk@hotmail.com>
2025-06-11 18:47:10 +0200

    Update flag.py

--- flag.py
+++ flag.py
@@ -21,12 +21,6 @@ JAWG_TOKEN = os.getenv('JAWG_TOKEN') or "f2wwvI5p3NCM9DJXW3xs7LZLcaY6AM9HKMYxlxd
 COPERNICUS_USERNAME = os.getenv('COPERNICUS_USERNAME')
 COPERNICUS_PASSWORD = os.getenv('COPERNICUS_PASSWORD')
 
-st.set_page_config(
-    page_title="Blue Flag Beaches of Greece",
-    page_icon="🌊",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
 
 # ─────────────────────────────────────────────────────────────────────────────
 # 2) Load pre-generated depth database
