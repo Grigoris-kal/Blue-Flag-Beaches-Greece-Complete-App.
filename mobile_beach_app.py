f0145bf1427617259bb23d229126fd6d422ab4a1
Grigoris-kal <grigorisk@hotmail.com>
2025-07-01 16:06:55 +0200

    Update mobile_beach_app.py

--- mobile_beach_app.py
+++ mobile_beach_app.py
@@ -196,7 +196,7 @@ def create_mobile_map(df, weather_cache, depth_data):
     )
     
     return pdk.Deck(
-        map_style='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        map_style='mapbox://styles/mapbox/outdoors-v11',
         initial_view_state=pdk.ViewState(
             latitude=39.0742,   # Center of Greece
             longitude=21.8243,  # Center of Greece  
